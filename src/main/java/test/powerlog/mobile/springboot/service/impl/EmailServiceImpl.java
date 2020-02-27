3fb9ed333e70d8fb3fd1aea4f64c086c1c9e6fb3
Seungyeon Kang <tmddusgood@gmail.com>
2020-02-27 19:16:03 +0900

    Revert "Application extends Servlet initializer"
    This reverts commit e8d38bb7

--- src/main/java/test/powerlog/mobile/springboot/service/impl/EmailServiceImpl.java
+++ src/main/java/test/powerlog/mobile/springboot/service/impl/EmailServiceImpl.java
@@ -15,8 +15,9 @@ import java.util.Date;
 @Service
 public class EmailServiceImpl implements EmailService {
 
-//    @Autowired
+    @Autowired
     private JavaMailSender javaMailSender;
+
     @Override
     public void sendMail(EmailFormDto email) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage();
