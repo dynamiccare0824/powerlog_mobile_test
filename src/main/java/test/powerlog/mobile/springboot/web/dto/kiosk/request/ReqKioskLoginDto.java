fd734069d260aa6565503e70cbf423689f13eec3
Seungyeon Kang <tmddusgood@gmail.com>
2020-03-01 20:43:45 +0900

    REST API prototype - TimeSet Req/RspKioskLoginDto corrected

--- src/main/java/test/powerlog/mobile/springboot/web/dto/kiosk/request/ReqKioskLoginDto.java
+++ src/main/java/test/powerlog/mobile/springboot/web/dto/kiosk/request/ReqKioskLoginDto.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
@@ -14,7 +15,7 @@ import javax.validation.constraints.Size;
 @AllArgsConstructor
 @ApiModel(value = "", description = "키오스크에서 uid를 받아 계정을 특정하기 위한 객체")
 public class ReqKioskLoginDto {
-    @NotEmpty(message = "휴대폰 번호를 공백없이 올바르게 입력해주세요")
+    @NotBlank(message = "고유번호 12자리를 공백없이 입력해주세요.")
     @Size(min = 12, max = 12, message = "고유번호는 12자리 입니다.")
     @ApiModelProperty(value = "고유 번호", required = true, position = 1, example = "0123456789*#")
     private String uid;
