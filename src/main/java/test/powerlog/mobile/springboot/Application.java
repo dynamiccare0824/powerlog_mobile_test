0f9391535275b2bf1bd53db5527626f6f67bff54
Seungyeon Kang <tmddusgood@gmail.com>
2020-03-01 16:17:03 +0900

    REST API prototype - exclud SecurityAutoConfiguration in Application

--- src/main/java/test/powerlog/mobile/springboot/Application.java
+++ src/main/java/test/powerlog/mobile/springboot/Application.java
@@ -2,10 +2,11 @@ package test.powerlog.mobile.springboot;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 //public class Application extends SpringBootServletInitializer {
 public class Application {
 
