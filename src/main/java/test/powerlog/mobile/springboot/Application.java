4f3bb698fbfa1bdd202cbd21797eab51041c35bd
Seungyeon Kang <tmddusgood@gmail.com>
2020-03-01 16:10:42 +0900

    REST API prototype - not extending initializer in application

--- src/main/java/test/powerlog/mobile/springboot/Application.java
+++ src/main/java/test/powerlog/mobile/springboot/Application.java
@@ -6,7 +6,8 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+//public class Application extends SpringBootServletInitializer {
+public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
