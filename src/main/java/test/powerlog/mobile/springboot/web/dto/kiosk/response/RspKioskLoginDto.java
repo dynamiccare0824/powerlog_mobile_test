55c776fc1e0d8d28f62b261c8a7aba1a2cc5f656
Seungyeon Kang <tmddusgood@gmail.com>
2020-03-01 20:38:36 +0900

    REST API prototype - TimeSet Failed on EC2, Kiosk Controller and package arranged

--- src/main/java/test/powerlog/mobile/springboot/web/dto/kiosk/response/RspKioskLoginDto.java
+++ src/main/java/test/powerlog/mobile/springboot/web/dto/kiosk/response/RspKioskLoginDto.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import test.powerlog.mobile.springboot.web.dto.common.CommonResponseDto;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.List;
@@ -16,7 +17,7 @@ import java.util.List;
 @AllArgsConstructor
 @ApiModel(value = "", description = "키오스크에서 uid를 받아 계정을 특정하기 위한 객체")
 public class RspKioskLoginDto<T> extends CommonResponseDto {
-    @NotEmpty(message = "고유번호 12자리를 공백없이 입력해주세요.")
+    @NotBlank(message = "고유번호 12자리를 공백없이 입력해주세요")
     @Size(min = 12, max = 12, message = "고유번호 12자리를 공백없이 입력해주세요.")
     @ApiModelProperty(value = "고유 번호", required = true, position = 1, example = "0123456789*#")
     private Boolean isPresent;
