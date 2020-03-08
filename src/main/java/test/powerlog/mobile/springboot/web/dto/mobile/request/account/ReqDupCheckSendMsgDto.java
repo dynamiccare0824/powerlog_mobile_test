e5c4aeb2989e608896bc135016a4266bd6018adb
dtmddusgood <tmddusgood@gmail.com>
2020-03-08 16:09:16 +0900

    dupcheck phone patternize

--- src/main/java/test/powerlog/mobile/springboot/web/dto/mobile/request/account/ReqDupCheckSendMsgDto.java
+++ src/main/java/test/powerlog/mobile/springboot/web/dto/mobile/request/account/ReqDupCheckSendMsgDto.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
@@ -15,7 +16,8 @@ import javax.validation.constraints.Size;
 @ApiModel(value = "", description = "Account 관련 request에서 자주 쓰이는 파라미터값을 모아만든 객체")
 public class ReqDupCheckSendMsgDto {
     @NotEmpty(message = "휴대폰 번호를 공백없이 올바르게 입력해주세요")
-    @Size(min = 10, max = 11, message = "휴대폰 번호를 올바르게 입력해주세요")
+    @Pattern(regexp="(?=.*[0-9]).{10,11}",
+            message = "휴대폰 번호를 공백없이 올바르게 입력해주세요")
     @ApiModelProperty(value = "핸드폰 번호", required = true, position = 1, example = "010123456789")
     private String phone;
 }
