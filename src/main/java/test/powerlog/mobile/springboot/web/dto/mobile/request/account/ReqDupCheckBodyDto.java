1ca020e9279698473bb2c0f05871c72de676931f
dtmddusgood <tmddusgood@gmail.com>
2020-03-08 16:42:58 +0900

    body detail message fixed ver2

--- src/main/java/test/powerlog/mobile/springboot/web/dto/mobile/request/account/ReqDupCheckBodyDto.java
+++ src/main/java/test/powerlog/mobile/springboot/web/dto/mobile/request/account/ReqDupCheckBodyDto.java
@@ -12,27 +12,28 @@ import javax.validation.constraints.*;
 @ApiModel(value = "", description = "회원가입시 이메일 체크 하는 부분에서 필요한 객체")
 public class ReqDupCheckBodyDto {
     @NotBlank(message = "공백없이 입력하세요.")
-    @ApiModelProperty(value = "이름", required = true, position = 1, example = "강승연")
+    @ApiModelProperty(value = "이름", required = true, position = 1, example = "male")
     private String gender;
 
     @NotBlank(message = "공백없이 입력하세요.")
     @DecimalMin(value = "19000000", message = "올바른 생년월일을 입력해주세요.")
     @DecimalMax(value = "20200306", message = "올바른 생년월일을 입력해주세요.")
-    @Pattern(regexp="(?=.*[0-9]){8}",
+    @Pattern(regexp="(?=.*[0-9]).{8}",
             message = "올바른 값을 입력해주세요")
-    @ApiModelProperty(value = "생년월일", required = true, position = 2, example = "tmddusgood@gmail.com")
+    @ApiModelProperty(value = "생년월일", required = true, position = 2, example = "19951227")
     private String birth;
 
-    @Pattern(regexp="(?=.*[0-9]){2,3}",
+    @Pattern(regexp="(?=.*[0-9]).{2,3}",
             message = "올바른 값을 입력해주세요")
-    @DecimalMin(value = "50")
-    @DecimalMax(value = "300")
-    private int height;
+//    @DecimalMin(value = "50")
+//    @DecimalMax(value = "300")
+    @ApiModelProperty(value = "키", required = true, position = 3, example = "180")
+    private String height;
 
-    @Pattern(regexp="(?=.*[0-9]){2,3}",
+    @Pattern(regexp="(?=.*[0-9]).{2,3}",
             message = "올바른 값을 입력해주세요.")
     @DecimalMin(value = "10", message = "올바른 값을 입력해주세요.")
     @DecimalMax(value = "999")
-    @ApiModelProperty(value = "몸무게", required = true, position = 5, example = "111111")
-    private int weight;
+    @ApiModelProperty(value = "몸무게", required = true, position = 4, example = "85")
+    private String weight;
 }
